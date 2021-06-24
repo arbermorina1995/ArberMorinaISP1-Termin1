74c910add4e75234d8a3a71b903611567e3f5507
arbermorina1995 <56584776+arbermorina1995@users.noreply.github.com>
2021-06-24 23:03:28 +0200

    added a new class according to the uml scheme

---
+++ src/AllClassesForISP1Project/Result.java
@@ -0,0 +1,56 @@
+package AllClassesForISP1Project;
+
+public class Result {
+
+    //attributes for this associationclass, these are private
+
+    private double result;
+    private String letterGrade;
+
+
+    //getters and setter for these attributes
+
+
+    public double getResult (){
+
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result= result;
+
+    }
+
+    public String getLetterGrade (){
+        return letterGrade;
+
+    }
+
+    public void setLetterGrade(String letterGrade){
+        this.letterGrade = letterGrade;
+
+    }
+
+
+    //the constructor for this class
+    public Result (double result, String letterGrade){
+        this.result = result;
+        this.letterGrade= letterGrade;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

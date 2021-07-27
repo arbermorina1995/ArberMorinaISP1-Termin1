a3ad9ff9c6894d211a8fc3728eee235b9418c1f4
arbermorina1995 <56584776+arbermorina1995@users.noreply.github.com>
2021-07-27 20:59:46 +0200

    small error

--- src/AllClassesForISP1Project/Program.java
+++ src/AllClassesForISP1Project/Program.java
@@ -9,21 +9,6 @@ public class Program {
 
     public static void main (String[] args){
 
-        //down below is the JFrame which is a GUI window with added components
-
-        /* JFrame frame = new JFrame(); // this creates our frame --> boilerplate (standard code for a frame)
-
-        frame.setTitle("Contoso University"); // sets the title to the frame of GUI
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// pressing X will shut down the program
-
-
-        frame.setSize(500,500); //sets the x and y dimension
-        frame.setResizable(false); //preventing the program to change dimensions of size
-        frame.setVisible(true); // set it to true, this will make our frame visible.
-
-        ImageIcon imageLogo = new ImageIcon("armoCoding.png");
-        frame.setIconImage(imageLogo.getImage());
-        frame.getContentPane().setBackground(new Color(153, 99, 255, 255)); // this changes my background color */
 
         LoginForm loginForm = new LoginForm(); // calling an instance of the class loginForm.
 
@@ -41,7 +26,7 @@ public class Program {
 
 
 
-        Student s1 = new Student("ar1834mo-s","Arber Morina");
+       /*Student s1 = new Student("ar1834mo-s","Arber Morina");
         Student s2 = new Student("fi2000mo-s", "Fidaim Morina");
         Student s3 = new Student("li1991mo-s", "Linda Morina");
         Student s4 = new Student("ek1969mo-s","Ekrem Morina");
@@ -49,16 +34,9 @@ public class Program {
         Student[] students = {s1,s2,s3,s4};
 
         WrittenExam we = new WrittenExam("Mjukvaruarkitektur",20,"Lund",8, 100);
-        Course sysa14 = new Course("Sysa14", "Programkonstruktion",4);
+        Course sysa14 = new Course("Sysa14", "Programkonstruktion",4);*/
 
 
-
-        // TODO: 2021-07-05 Nu har jag gjort så att jag idag 5/7 fixade associationsklassen
-        // TODO: 2021-07-05 detta gjorde jag genom implementera i både writtenexam klass och student klass då associations
-        //klassen är mellan dom två klasserna.
-        // TODO: 2021-07-05  skapade linkedList<Result> results; i både written och student.
-        // se koden.
-        // todo---> nu kolla tutorial mvc och crud och java form. metoder också ifall de e klara.
 
 
 
--- src/View/LoginForm.java
+++ src/View/LoginForm.java
@@ -7,25 +7,48 @@ public class LoginForm extends JFrame {
 
     public LoginForm(){ // constructor
 
+        JPanel jPanel = new JPanel();
 
 
-        this.setTitle("Contoso University"); // sets the title to the this of GUI
+        this.setTitle("Contoso University"); // sets the title to the this of frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// pressing X will shut down the program
-
-
         this.setSize(500,500); //sets the x and y dimension
         this.setResizable(false); //preventing the program to change dimensions of size
-        this.setVisible(true); // set it to true, this will make our this visible.
+
 
         ImageIcon imageLogo = new ImageIcon("armoCoding.png");
         this.setIconImage(imageLogo.getImage());
-        this.getContentPane().setBackground(new Color(153, 99, 255, 255)); // this changes my background color
+        //this.getContentPane().setBackground(new Color(153, 99, 255, 255)); // this changes my background color
 
-        JLabel label = new JLabel();
-        label.setText("Välkommen till Contoso University");
-        this.add(label);
+        JLabel label1 = new JLabel();
+        label1.setText("Välkommen till Contoso University");
+        this.add(label1);
+        label1.setHorizontalTextPosition(JLabel.CENTER);
+        label1.setVerticalTextPosition(JLabel.TOP);
 
-        getContentPane();
+
+        this.setVisible(true); // set it to true, this will make our this visible.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
